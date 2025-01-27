7a09ea68a7a9f78148a7a3eb5190320cae1981e3
snoww. <155428502+snowded@users.noreply.github.com>
2025-01-27 13:34:45 +0500

    Update pay.js

--- commands/pay.js
+++ commands/pay.js
@@ -2,6 +2,9 @@ const { EmbedBuilder } = require('discord.js');
 
 exports.run = async (client, message, args) => {
 
+
+const pay = 'Your Details';
+
     // Create the embed
     const embed = new EmbedBuilder()
    .setDescription(pay);
